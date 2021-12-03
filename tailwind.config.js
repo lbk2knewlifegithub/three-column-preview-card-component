605d22305eed7d4408b5259ad650935a3003d21a
lbk2knewlifegithub <lbk2knewlife@gmail.com>
2021-12-03 20:10:48 +0700

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,13 +25,6 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        lg: "4rem",
-        xl: "5rem",
-        "2xl": "6rem",
-      },
     },
     extend: {
       // create custom fonts here
